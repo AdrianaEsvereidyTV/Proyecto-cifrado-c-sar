c992b3b3ea94e8d5aef2038df00313363dc3d50f
Adriana <nossa.musica.07.10@gmail.com>
2018-06-18 00:48:14 -0500

    Terminando la funcionalidad

---
+++ src/READMETots.md
@@ -0,0 +1,3 @@
+# Fans de Studio Ghibli (スタジオジブリ)
+
+Habrá un evento pequeño intercambio de mensajes para los fans de Studio Ghibli en memoria del reciente fallecimiento de unos de los fundadores el maestro [Isao Takahata] (https://es.wikipedia.org/wiki/Isao_Takahata),  
--- src/cipher.js
+++ src/cipher.js
@@ -2,12 +2,12 @@ window.cipher = {
  offset: 33,
 
 encode: (offset,string) => {
-  let codificado= "";
-  let offsetEncode = parseInt(offset);
-  for (let i = 0; i< string.length; i++){
+  let codificado= ""; //Se crea la variable del mensaje
+  let offsetEncode = parseInt(offset); // Se cra la variable y se le da el valor de número
+  for (let i = 0; i< string.length; i++){ //Se crea el bucle para determinar en código ASCII
     let codeRin = string.charCodeAt(i); 
-    if (codeRin >= 65 && codeRin <= 90){
-      codificado += String.fromCharCode((codeRin -65 + offsetEncode)%26+65);
+    if (codeRin >= 65 && codeRin <= 90){ //Fórmula 
+      codificado += String.fromCharCode((codeRin -65 + offsetEncode)%26 + 65);
      
     } else if (codeRin >= 97 && codeRin <= 122){
       codificado += String.fromCharCode((codeRin -97 + offsetEncode)% 26 + 97);
@@ -15,6 +15,9 @@ encode: (offset,string) => {
     } else if (codeRin === 32){
       codificado += String.fromCharCode(codeRin);
       
+    } else if (codeRin >=33 && codeRin <=64){
+      codificado += String.fromCharCode((codeRin -33 + offsetEncode)%26 + 33);
+
     }
     console.log (codificado);
   }
@@ -35,13 +38,16 @@ decode: (offset,string) =>{
     }else if (decodeRin === 32) {
      descifrado += String.fromCharCode(decodeRin);
      
+    }else if (decodeRin >=33 && decodeRin <=64){
+      descifrado += String.fromCharCode((decodeRin -64 - offsetDecode)% 26 + 64);
+      
     }
     console.log(descifrado);
- }
- return descifrado;
-},
-createCipherWithOffset: (offsetHackerEdition)=>{
+  }
+  return descifrado;
+ },
+createCipherWithOffset: (offsetHackerEdition,string)=>{
 
-} 
+ }
 
 };
